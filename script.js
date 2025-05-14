b9a303dddde5f9ef2775af9434ec0b78bc14a373
jaqp1 <kuba.wenek@wp.pl>
2025-05-14 18:18:33 +0200

    Fixed bugs

--- script.js
+++ script.js
@@ -12,10 +12,21 @@ let swistakGeneratorTimeout = null;
 let stop = true;
 let counterInterval = null;
 
+
 btnStart.addEventListener("click", () => {
+  //btnStop.disabled = true;
+  //btnStart.disabled = true;
+  
+
   clearInterval(intervalId);
   clearTimeout(randomHolesTimeout);
   clearTimeout(swistakGeneratorTimeout);
+  clearTimeout(gameOver);
+
+  // let stopFreeze = setTimeout(() => {
+  //   btnStop.disabled = false;;
+  // },5000);
+  //  clearTimeout(stopFreeze);
 
   let counter = 5;
   let h1 = document.createElement("h1");
@@ -62,6 +73,8 @@ btnStart.addEventListener("click", () => {
     },6000 );
 
     stop = false;
+    
+   
   }
   
 
@@ -88,7 +101,16 @@ btnStop.addEventListener("click", () => {
     clearTimeout(randomHolesTimeout);
     clearTimeout(swistakGeneratorTimeout);
     stop = true;
-}});
+    score = 0;
+    scoreString = "Score: "+ score;
+    document.querySelector(".score").textContent = scoreString;
+
+    document.querySelectorAll(".life_points_img").forEach(life => {
+      life.src = "zwierzak.png";
+    });
+  }
+  fotoCounter = 5
+});
 
 btnReset.addEventListener("click", () => {
   randomSwistakGenerator();
@@ -147,6 +169,8 @@ function generateHoles(){
 function randomSwistakGenerator(){
   document.querySelectorAll(".swistak").forEach(swistak => {
     swistak.remove();
+    document.querySelector(`.life_points_img${fotoCounter}`).src = "zwierzak_down.png";
+      fotoCounter--;
   });
   swistak.style.position = "absolute";
   randomPosition = Math.floor(Math.random() * (positions.length ));
@@ -154,5 +178,73 @@ function randomSwistakGenerator(){
   swistak.style.top = positions[randomPosition].y - 60 + "px";
   swistak.style.width = "200px";
   swistak.style.height = "200px";
+
+  stopGamePane();
   container.appendChild(swistak);
+}
+  
+
+let score = 0;
+let scoreString = "Score: "+ score;
+let fotoCounter = 5;
+let gameOver;
+document.querySelector(".container").addEventListener("click", (event) => {
+  if(event.target.classList.contains("swistak")){
+      clicked = true;
+      score++;
+      let scoreString = "Score: "+ score;
+      document.querySelector(".score").textContent = scoreString;
+      document.querySelectorAll(".swistak").forEach(swistak => {
+      swistak.remove();
+    });
+    } else if (event.target.classList.contains("buttons")) {
+      return;
+    }else if (!stop){
+      document.querySelector(`.life_points_img${fotoCounter}`).src = "zwierzak_down.png";
+      fotoCounter--;
+    }
+  stopGamePane();
+  }
+);
+
+function stopGamePane(){
+  if(fotoCounter < 1){
+    btnStop.disabled = true;
+    clearInterval(intervalId);
+    clearTimeout(randomHolesTimeout);
+    clearTimeout(swistakGeneratorTimeout);
+        let h1 = document.createElement("h1");
+        h1.textContent = "Game Over";
+        h1.style.position = "absolute";
+        h1.style.left = "50%";
+        h1.style.top = "60%";
+        h1.style.transform = "translate(-50%, -50%)";
+        h1.style.fontSize = "200px";
+        h1.style.color = "#e5bd68";
+        h1.style.zIndex = "1000";
+        h1.style.transition = "opacity 0.5s ease";
+        h1.style.opacity = "1";
+        backdrop.appendChild(h1);
+
+    gameOver = setTimeout(() => {
+      h1.style.opacity = "0";
+      h1.remove();
+
+      box.style.transition = "transform 0.5s ease, opacity 0.5s ease";
+      box.style.transform = "translateY(0%)";
+      box.style.opacity = "1";
+      backdrop.classList.remove("transparent");
+      document.querySelector("body").style.backgroundImage = "url('plansza.png')";
+      document.querySelectorAll(".hole").forEach(hole => {
+      hole.remove();
+    });
+    document.querySelectorAll(".swistak").forEach(swistak => {
+      swistak.remove();
+    });
+    document.querySelectorAll(".life_points_img").forEach(life => {
+      life.src = "zwierzak.png";
+    });
+    btnStart.disabled = false;
+  }, 5000);
+  }
 }
